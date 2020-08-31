e507c23b0b75250d4afacf49ef38cf1cf47a29f4
andrencam <naka.campos05@gmail.com>
2020-08-31 14:49:18 -0700

    new files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAElyGGACuxUZLOaGcDACTW3xgphjFJR24",
+  authDomain: "facebook-c19c9.firebaseapp.com",
+  databaseURL: "https://facebook-c19c9.firebaseio.com",
+  projectId: "facebook-c19c9",
+  storageBucket: "facebook-c19c9.appspot.com",
+  messagingSenderId: "718775468734",
+  appId: "1:718775468734:web:aeebcf1e411d40141554ef",
+  measurementId: "G-93R1HZ50CW",
+};
